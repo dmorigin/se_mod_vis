4e7c78b56eeb5290c0d251e7f071bb10d0eb4649
dmorigin <stephan@kirfel-koeln.de>
2020-01-14 23:26:28 +0100

    Changed: Instruction count in statistics is back. Removed: Call statistics

--- VisualInformationSystem/Statistics.cs
+++ VisualInformationSystem/Statistics.cs
@@ -61,7 +61,7 @@ namespace IngameScript
 
             StringBuilder sb_ = new StringBuilder();
 
-            long callChainCountLastUpdate_ = 0;
+            long instructionCountLastUpdate_ = 0;
             long ticksSinceLastUpdate_ = 0;
             double timeSinceLastUpdate_ = 0.0;
 
@@ -70,7 +70,7 @@ namespace IngameScript
                 ticks_ += app.Runtime.TimeSinceLastRun;
 
                 // update
-                callChainCountLastUpdate_ += app.Runtime.CurrentCallChainDepth;
+                instructionCountLastUpdate_ += app.Runtime.CurrentInstructionCount;
                 timeSinceLastUpdate_ += app.Runtime.LastRunTimeMs * sensitivity_;
                 ticksSinceLastUpdate_++;
 
@@ -89,7 +89,7 @@ namespace IngameScript
                     sb_.AppendLine($"Time: {ticks_}");
                     sb_.AppendLine($"Ticks: {ticksSinceLastUpdate_}");
                     sb_.AppendLine($"Avg Time/tick: {(timeSinceLastUpdate_ / ticksSinceLastUpdate_).ToString("#0.0#####")}ms");
-                    sb_.AppendLine($"Avg Call/tick: {(callChainCountLastUpdate_ / (double)ticksSinceLastUpdate_).ToString("#0.00")}/{app.Runtime.MaxCallChainDepth}");
+                    sb_.AppendLine($"Avg Inst/tick: {(instructionCountLastUpdate_ / (double)ticksSinceLastUpdate_).ToString("#0.00")}/{app.Runtime.MaxInstructionCount}");
                     sb_.AppendLine($"Job (Timed): {jobTimed}");
                     sb_.AppendLine($"Job (Queue/Exec): {jobQueued}/{jobQueuedExec}");
 
@@ -99,7 +99,7 @@ namespace IngameScript
                     app.Echo(sb_.ToString());
 
                     nextUpdate_ = ticks_ + updateInterval_;
-                    callChainCountLastUpdate_ = 0;
+                    instructionCountLastUpdate_ = 0;
                     ticksSinceLastUpdate_ = 0;
                     timeSinceLastUpdate_ = 0.0;
                 }
