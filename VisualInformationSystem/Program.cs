7338c00407df02b92f41adb2e3af74cd289e99f1
dmorigin <stephan@kirfel-koeln.de>
2020-01-14 21:32:50 +0100

    Changed Version to v0.21

--- VisualInformationSystem/Program.cs
+++ VisualInformationSystem/Program.cs
@@ -22,7 +22,7 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        const string VERSION = "0.2";
+        const string VERSION = "0.21";
 
         static Program App = null;
 
